f9791dbfad0921f8fb569c0509d2edd5a0bf62d4
rafia9005 <rafia9005@gmail.com>
2025-02-23 12:50:08 +0700

    fix

---
+++ resources/views/admin-penulis-update.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+
+@section('content')
+    <main class="flex-1">
+        <h1 class="text-xl font-bold mb-5">Edit Penulis</h1>
+        <div class="bg-white shadow-md rounded-lg p-6">
+            <form method="POST" class="space-y-4">
+                @csrf
+
+                <div>
+                    <label for="nama" class="block text-sm font-medium text-gray-700">Nama Penulis</label>
+                    <input type="text" id="nama" name="nama"
+                        class="mt-1 block w-full px-4 py-2 border rounded-lg shadow-sm focus:ring focus:ring-blue-300 focus:outline-none">
+                </div>
+
+                <div class="flex items-center space-x-4">
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Simpan</button>
+                    <a class="text-gray-600 hover:underline">Batal</a>
+                </div>
+            </form>
+        </div>
+    </main>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\SiswaController;
+use Illuminate\Support\Facades\Route;
+
+/* |--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get("/login", [AuthController::class, 'login'])->name('login');
+Route::get("/register", [AuthController::class, 'register']);
+
+Route::prefix("/dashboard")->group(function () {
+    Route::get("/siswa", [SiswaController::class, 'siswa_dashboard'])->name('siswa_dashboard');
+    Route::get('/siswa/peminjaman', [SiswaController::class, 'siswa_dashboard_create_peminjaman'])->name('siswa_dashboard_create_peminjaman');
+    Route::get("/siswa/buku", [SiswaController::class, 'siswa_dashboard_buku'])->name('siswa_dashboard_buku');
+
+    Route::get('/admin', [AdminController::class, 'admin_dashboard'])->name('admin_dashboard');
+    Route::get('/admin/create-book', [AdminController::class, 'admin_create_buku_dashboard'])->name('admin_create_buku_dashboard');
+    Route::get('/admin/update-book', [AdminController::class, 'admin_update_buku_dashboard'])->name('admin_update_buku_dashboard');
+    Route::get('/admin/penulis', [AdminController::class, 'admin_penulis_dashboard'])->name('admin_penulis_dashboard');
+    Route::get('/admin/penulis/update', [AdminController::class, 'admin_penulis_update_dashboard'])->name('admin_penulis_update_dashboard');
+    Route::get('/admin/penerbit', [AdminController::class, 'admin_penerbit_dashboard'])->name('admin_penerbit_dashboard');
+    Route::get('/admin/penerbit/update', [AdminController::class, 'admin_penerbit_update_dashboard'])->name('admin_penerbit_update_dashboard');
+    Route::get('/admin/peminjaman', [AdminController::class, 'admin_peminjaman_dashboard'])->name('admin_peminjaman_dashboard');
+    Route::get('/admin/kategori', [AdminController::class, 'admin_kategori_dashboard'])->name('admin_kategori_dashboard');
+    Route::get('/settings', [SettingsController::class, 'pengaturan'])->name('pengaturan');
+});
